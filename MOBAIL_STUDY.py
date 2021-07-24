f4f92fb6ed43b6a64836824cb640fa589a875e94
dijaidy <65172181+dijaidy@users.noreply.github.com>
2021-07-25 02:39:25 +0900

    infra modification
    have to modify mouse checking

--- MOBAIL_STUDY.py
+++ MOBAIL_STUDY.py
@@ -9,6 +9,8 @@ green = (128, 0, 0)
 blue = (000, 000, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
+light_blue = (121, 220, 255)
+light_green = (159, 255, 63)
 
 # 기본 초기화
 pygame.init()
@@ -35,54 +37,44 @@ each_scene_dict = {}
 
 
 class Caption:
-    # click_checking_rect의 정보 표현할때 쓰이는 상수
-    rect_x_constant = 2
-    rect_y_constant = 1
+    caption_bundle_dict = {}
     # 텍스트 생성 시 모든 정보(크기, 입력할 글, 폰트, 색깔, 위치) 입력
     def __init__(
-        self, text, pos, size, scene, right_below_dot_pos=(0, 0), font="BMJUA_ttf.ttf", color=black
+        self, text, pos, size, right_below_dot_pos=(0, 0), font="BMJUA_ttf.ttf", color=black
     ):
-
+        # 일반 인스턴스 선언
         self.__text = text
         self.__size = size
         self.__font = font
         self.__color = color
         self.__pos = pos
-        self.__scene = scene
 
-        if not type(text) == str:  # 객체 안에 글이 여러개면
-            self.click_checking_rect = []
-            for i in range(0, len(text)):
-                self.click_checking_rect.append(
-                    ((pos[i][0], right_below_dot_pos[i][0]), (pos[i][1], right_below_dot_pos[i][1]))
-                )
-        else:
-            self.click_checking_rect = (
-                (pos[0], right_below_dot_pos[0]),
-                (pos[1], right_below_dot_pos[1]),
-            )
+        # 특수한 인스턴스 선언
 
-        if self.__scene not in each_scene_dict:
-            each_scene_dict[self.__scene] = [self]
-        else:
-            each_scene_dict[self.__scene].append(self)
-
-    def get_text(self):
-        return self.__text
+        # 클릭을 체크하는 네모에 관한 정보
+        self.click_checking_rect = (
+            (pos[0], right_below_dot_pos[0]),
+            (pos[1], right_below_dot_pos[1]),
+        )
 
     # 텍스트 그리기
     def write_caption(self):
 
-        if type(self.__text) == str:  # 한 줄 렌더 시
-            caption_font = pygame.font.Font(self.__font, self.__size)
-            final_caption = caption_font.render(self.__text, True, self.__color)
-            screen.blit(final_caption, self.__pos)
+        caption_font = pygame.font.Font(self.__font, self.__size)
+        final_caption = caption_font.render(self.__text, True, self.__color)
+        screen.blit(final_caption, self.__pos)
 
-        else:  # 여러 줄 렌더 시(size, text, pos, color 모두 튜플형태로 가져와야함)
-            for i in range(0, len(self.__text)):
-                caption_font = pygame.font.Font(self.__font, self.__size[i])
-                final_caption = caption_font.render(self.__text[i], True, self.__color[i])
-                screen.blit(final_caption, self.__pos[i])
+    def sort_caption(self, scene_of_object, caption_bundle):
+        # 어느 장면인지(ex:시작장면, 내교재찜하기 장면 등)
+        if scene_of_object not in each_scene_dict:
+            each_scene_dict[scene_of_object] = [self]
+        else:
+            each_scene_dict[scene_of_object].append(self)
+
+        if caption_bundle not in Caption.caption_bundle_dict:
+            Caption.caption_bundle_dict[caption_bundle] = [self]
+        else:
+            Caption.caption_bundle_dict[caption_bundle].append(self)
 
 
 class BigCategory(Caption):
@@ -97,45 +89,32 @@ class BigCategory(Caption):
         self,
         text,
         pos,
-        scene,
-        linked_small_category,
         right_below_dot_pos=(0, 0),
         size=20,
         font="BMJUA_ttf.ttf",
         color=black,
     ):
 
+        # 일반 인스턴스 선언
         self.__text = text
         self.__size = size
         self.__font = font
         self.__color = color
         self.__pos = pos
-        self.__scene = scene
 
-        if not type(text) == str:
-            self.click_checking_rect = []
-            for i in range(0, len(text)):
-                self.click_checking_rect.append(
-                    ((pos[i][0], right_below_dot_pos[i][0]), (pos[i][1], right_below_dot_pos[i][1]))
-                )
-        else:
-            self.click_checking_rect = (
-                (pos[0], right_below_dot_pos[0]),
-                (pos[1], right_below_dot_pos[1]),
-            )
+        self.click_checking_rect = (
+            (pos[0], right_below_dot_pos[0]),
+            (pos[1], right_below_dot_pos[1]),
+        )
 
-        self.__link = linked_small_category  # 하위카테고리를 연결
+        # 작은 카테고리 링크 작업
+        self.__linked_small_category = Caption.caption_bundle_dict[self.__text]
 
         # 처음 정의되는 큰 카테고리에 대한 활성화 작업
         if BigCategory.activated_category == 0:  # 아직 활성화된 카테고리 없으면
             BigCategory.activated_category = self  # 처음 정의되는 객체를 활성화(기존의 객체 자체가 변화함(파괴적임))
             BigCategory.deactivated_font_size = self.__size  # 기존의 폰트 사이즈 미리 저장
             BigCategory.activated_category.__size = BigCategory.activated_font_size  # 글씨크기는 키우기
-
-        if self.__scene not in each_scene_dict:
-            each_scene_dict[self.__scene] = [self]
-        else:
-            each_scene_dict[self.__scene].append(self)
 
     def activate_category(self):
         # 기존의 활성화 카테고리에 대한 작업
@@ -147,20 +126,15 @@ class BigCategory(Caption):
 
     def write_caption(self):
         # 큰 카테고리 그림
-        if type(self.__text) == str:  # 한 줄 렌더 시
-            caption_font = pygame.font.Font(self.__font, self.__size)
-            final_caption = caption_font.render(self.__text, True, self.__color)
-            screen.blit(final_caption, self.__pos)
-
-        else:  # 여러 줄 렌더 시(size, text, pos, color 모두 튜플형태로 가져와야함)
-            for i in range(0, len(self.__text)):
-                caption_font = pygame.font.Font(self.__font, self.__size[i])
-                final_caption = caption_font.render(self.__text[i], True, self.__color[i])
-                screen.blit(final_caption, self.__pos[i])
+        caption_font = pygame.font.Font(self.__font, self.__size)
+        final_caption = caption_font.render(self.__text, True, self.__color)
+        screen.blit(final_caption, self.__pos)
 
         # 현재 객체가 활성화된 객체면
         if BigCategory.activated_category == self:
-            self.__link.write_caption()
+            # 링크된 작은카테고리 표시
+            for linked_small_category in self.__linked_small_category:
+                linked_small_category.write_caption()
             # 활성화된 큰 카테고리의 위치에 활성상태를 표시하는 박스 그림
             pygame.draw.rect(
                 screen,
@@ -180,7 +154,6 @@ class SmallCategory(Caption):
         self,
         text,
         pos,
-        scene,
         right_below_dot_pos=(0, 0),
         size=10,
         font="BMJUA_ttf.ttf",
@@ -191,34 +164,17 @@ class SmallCategory(Caption):
         self.__font = font
         self.__color = color
         self.__pos = pos
-        self.__scene = scene
 
-        if not type(self.__text) == str:
-            self.click_checking_rect = []
-            for i in range(0, len(self.__text)):
-                self.click_checking_rect.append(
-                    (
-                        (self.__pos[i][0], right_below_dot_pos[i][0]),
-                        (pos[i][1], right_below_dot_pos[i][1]),
-                    )
-                )
-        else:
-            self.click_checking_rect = (
-                (pos[0], right_below_dot_pos[0]),
-                (pos[1], right_below_dot_pos[1]),
-            )
-
-        if self.__scene not in each_scene_dict:
-            each_scene_dict[self.__scene] = [self]
-        else:
-            each_scene_dict[self.__scene].append(self)
+        self.click_checking_rect = (
+            (pos[0], right_below_dot_pos[0]),
+            (pos[1], right_below_dot_pos[1]),
+        )
 
     def write_caption(self):
-        print("object:", self.__text, "size:", self.__size)
-        for i in range(0, len(self.__text)):  # 큰 카테고리에 연결된 작은 카테고리 그림
-            caption_font = pygame.font.Font(self.__font, self.__size[i])
-            final_caption = caption_font.render(self.__text[i], True, self.__color[i])
-            screen.blit(final_caption, self.__pos[i])
+
+        caption_font = pygame.font.Font(self.__font, self.__size)
+        final_caption = caption_font.render(self.__text, True, self.__color)
+        screen.blit(final_caption, self.__pos)
 
     def scene_transform(self):
         pass
@@ -248,20 +204,25 @@ class Png:
 # 텍스트 객체 정의
 # 처음 dday 표시
 if True:  # 시험기간이면
-    d_day_exam_caption_x_pos = 20
-    d_day_exam_caption_y_pos = 80
-    d_day_exam_caption = Caption(
-        size=(20, 20),
-        text=("1학기 중간고사", "D-30"),
-        pos=(
-            (d_day_exam_caption_x_pos, d_day_exam_caption_y_pos),
-            (d_day_exam_caption_x_pos, d_day_exam_caption_y_pos + 20),
-        ),
-        color=(black, black),
-        scene="start_scene",
-        right_below_dot_pos=((0, 0), (0, 0)),
-    )
-    each_scene_dict["start_scene"].append(d_day_exam_caption)
+    d_day_exam_caption_x_pos_list = [20, 20]
+    d_day_exam_caption_y_pos_list = [80, 100]
+    d_day_exam_caption_text_list = ["1학기 중간고사", "D-30"]
+    d_day_exam_caption_pos_list = [
+        (d_day_exam_caption_x_pos_list[0], d_day_exam_caption_y_pos_list[0]),
+        (d_day_exam_caption_x_pos_list[0], d_day_exam_caption_y_pos_list[1]),
+    ]
+    d_day_exam_caption_right_below_dot_pos_list = [(50, 100), (40, 120)]
+
+    for i in range(0, 2):
+        d_day_exam_caption = Caption(
+            size=20,
+            text=d_day_exam_caption_text_list[i],
+            pos=d_day_exam_caption_pos_list[i],
+            color=black,
+            right_below_dot_pos=d_day_exam_caption_right_below_dot_pos_list[i],
+        )
+        d_day_exam_caption.sort_caption("start_scene", "d_day_exam_caption")
+        d_day_exam_caption.write_caption()
 
 
 # 큰 카테고리, 작은 카테고리 만들기
@@ -280,41 +241,35 @@ small_category_str_list = [
 # 작은 카테고리 위치
 small_category_x_pos = 20
 small_category_y_pos_list = [100, 120, 140, 160]
+small_category_right_below_dot_pos_list = [(50, 120), (50, 140), (50, 160), (50, 180)]
 
-for small_category_str in small_category_str_list:
-    small_category_pos_tuple = tuple(
-        [
-            (small_category_x_pos, small_category_y_pos_list[i])
-            for i in range(0, len(small_category_str))
-        ]
-    )  # 작은 카테고리의 위치정보
-    small_category = SmallCategory(
-        text=small_category_str,
-        pos=small_category_pos_tuple,
-        scene="start_scene",
-        right_below_dot_pos=((0, 0), (0, 0)),
-        size=tuple([10 for i in range(0, len(small_category_str))]),
-    )  # 수정필요
-    small_category_list.append(small_category)
-    each_scene_dict["start_scene"].append(small_category)
+caption_bundle_variable = 0
+for small_category_str_tuple in small_category_str_list:
+    for i in range(0, len(small_category_str_tuple)):  # 각각의 작은 카테고리에 대한 반복 작업
+        small_category_pos_tuple = tuple(
+            [
+                (small_category_x_pos, small_category_y_pos_list[i])
+                for i in range(0, len(small_category_str_tuple))
+            ]  # 작은 카테고리 개수에 따라 위치정보 양을 달리함
+        )  # 작은 카테고리의 위치정보
+        small_category = SmallCategory(
+            text=small_category_str_tuple[i],
+            pos=small_category_pos_tuple[i],
+            right_below_dot_pos=small_category_right_below_dot_pos_list[i],
+            size=10,
+        )  # 수정필요
+        small_category.sort_caption("start_scene", big_category_str_list[caption_bundle_variable])
+    caption_bundle_variable += 1
 
 
-for i in range(0, 5):
+for i in range(0, len(big_category_str_list)):
     big_category_x_pos = ((screen_width / 5) * i) - 30
     big_category_y_pos = 50
     big_category = BigCategory(
-        text=big_category_str_list[i],
-        pos=(big_category_x_pos, big_category_y_pos),
-        linked_small_category=small_category_list[i],
-        scene="start_scene",
-    )  # 수정필요
-    big_category_list.append(big_category)
-    each_scene_dict["start_scene"].append(big_category)
-
-# 텍스트 객체 생성
-d_day_exam_caption.write_caption()
-for big_category in big_category_list:
-    big_category.write_caption()
+        text=big_category_str_list[i], pos=(big_category_x_pos, big_category_y_pos)
+    )
+    big_category.sort_caption("start_scene", "big_category")
+    big_category.write_caption()  # 빅 카테고리를 그리면, 연결된 작은 카테고리도 함께 그림
 
 
 # 이미지 객체 정의
@@ -363,24 +318,26 @@ while running:
             running = False  # 게임 진행중이 아님
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             for object in each_scene_dict["start_scene"]:
-                if type(object) == SmallCategory or type(object) == BigCategory:
-                    if not object.get_text() == str:  # 텍스트가 여러개이면(작은 카테고리)
-                        for rect in object.click_checking_rect:
-                            if mouse_pos >= rect[0][0] and mouse_pos <= rect[0][1]:
-                                if (
-                                    mouse_pos >= rect[1][0] and mouse_pos <= rect[1][1]
-                                ):  # object를 클릭했으면
-                                    object.scene_transform(object.click_checking_rect.index(rect))
-                    else:  # 텍스트가 한개면(큰 카테고리)
+                if type(object) == SmallCategory:  # 텍스트가 여러개이면(작은 카테고리)
+                    if (
+                        mouse_pos >= object.click_checking_rect[0][0]
+                        and mouse_pos <= object.click_checking_rect[0][1]
+                    ):
                         if (
-                            mouse_pos >= object.click_checking_rect[0][0]
-                            and mouse_pos <= object.click_checking_rect[0][1]
-                        ):
-                            if (
-                                mouse_pos >= object.click_checking_rect[1][0]
-                                and mouse_pos <= object.click_checking_rect[1][1]
-                            ):  # object를 클릭했으면
-                                object.activate_category()
+                            mouse_pos >= object.click_checking_rect[1][0]
+                            and mouse_pos <= object.click_checking_rect[1][1]
+                        ):  # object를 클릭했으면
+                            object.scene_transform()
+                elif type(object) == BigCategory:  # 텍스트가 한개면(큰 카테고리)
+                    if (
+                        mouse_pos >= object.click_checking_rect[0][0]
+                        and mouse_pos <= object.click_checking_rect[0][1]
+                    ):
+                        if (
+                            mouse_pos >= object.click_checking_rect[1][0]
+                            and mouse_pos <= object.click_checking_rect[1][1]
+                        ):  # object를 클릭했으면
+                            object.activate_category()
 
     # 3. 게임 캐릭터 위치 정의
     # 5. 화면에 그리기
